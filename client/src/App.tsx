ed67572b380df65d32e4693856e2f9b45c5a6690
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-06-05 09:30:40 +0600

    app.tsx page problem slove

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,8 @@ export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Home/>} />
+                <Route path="/login" element={<Login/>} />
             </Routes>
         </>
     );
