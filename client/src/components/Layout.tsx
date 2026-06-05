888fdb59df8449e6cfdbfe84bc999b62cd380a06
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-06-05 16:26:47 +0600

    complete the conponent layout.tsx page

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import { useState } from 'react'
 import Sidebar from './Sidebar'
+import { Outlet, useLocation } from 'react-router-dom';
+import { MenuIcon } from 'lucide-react';
+
+
+const pageTitles : Record<string, string > = {
+    '/dashboard' : 'Dashboard',
+    '/accounts' : 'Accounts',
+    '/schedule' : 'Scheduler',
+    '/ai-composer' : 'AI Composer',
+}
 
 const Layout = () => {
+    const location= useLocation();
+    const title  = pageTitles[location.pathname] || 'SOCIAL AI'
+    const [isMobileMenuOpen , setIsMobileMenuOpen] = useState(false);
   return (
-    <div>
-        <Sidebar/>
+    <div className='flex h-screen bg-slate-50'>
+
+        {/* mobail overlay */}
+        {isMobileMenuOpen && <div className='fixed inset-0 bg-slate-900/50 z-40 md:hidden' onClick={()=> setIsMobileMenuOpen(false)}/>}
+        <Sidebar isOpen = {isMobileMenuOpen} setIsOpen = { setIsMobileMenuOpen}/>
+        <div className='flex-1 flex flex-col overflow-hidden'>
+            {/* top bar */}
+            <header className='h-16 bg-white border-b border-slate-200 flex items-center p-4 md:p-8 gap-4'>
+                <button className='md:hidden p-2 -ml-2 text-slate-500' onClick={()=> setIsMobileMenuOpen(true)}>
+                    <MenuIcon className='size-6'/>
+                </button>
+                <div>
+                    <h1 className='text-slate-900 capitalize'>{title}</h1>
+                    <p className='text-sm text-slate-400 hidden sm:block'>Manege and automate your social presence</p>
+                </div>
+            </header>
+            <main className='flex-1 overflow-auto p-4 sm:p-6 md:p-8 xl:p-12'>
+                <Outlet/>
+            </main>
+        </div>
     </div>
   )
 }
