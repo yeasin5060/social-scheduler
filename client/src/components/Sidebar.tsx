e03346a9e09bd253426ad5d2312091737e371f92
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-06-06 19:59:40 +0600

    complete the urse footer section in sidebar.tsx page

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,8 +1,17 @@
-import { CalendarDaysIcon, LayoutDashboardIcon, UserIcon } from 'lucide-react'
+import { CalendarDaysIcon, LayoutDashboardIcon, LogOutIcon, UserIcon } from 'lucide-react'
 import { NavLink, useLocation } from 'react-router-dom'
 
 const Sidebar = ({isOpen , setIsOpen} : {isOpen : boolean, setIsOpen : (val : boolean) => void }) => {
 
+    const {logout , user} = {
+        logout : () => {
+            window.location.href ='/'
+        },
+        user : {
+            name : 'yeasin munshi',
+            email : 'yeasin.dev01@gmail.com'
+        }
+    }
     const location = useLocation();
     const navItems = [
         {name : 'Dashboard', icon : LayoutDashboardIcon, path : '/dashboard'},
@@ -18,25 +27,46 @@ const Sidebar = ({isOpen , setIsOpen} : {isOpen : boolean, setIsOpen : (val : bo
                 <img src="/logo.svg" alt="logo not found" className='size-6'/>
                 Scheduler
             </div>
-             {/*  nav section label */}
-            <div className='px-6 py-2'>
-                <span className='text-xs text-slate-500 uppercase tracking-wider'>menu</span>
+        </div>
+        {/*  nav section label */}
+        <div className='px-6 py-2'>
+            <span className='text-xs text-slate-500 uppercase tracking-wider'>menu</span>
+        </div>
+
+        {/*  nav links */}
+        <nav className = 'flex-1 px-3 space-y-1'>
+            {navItems.map((item)=> { 
+                const isActive = location.pathname === item.path;
+
+                return (
+                    <NavLink className={`flex items-center gap-3 px-3 py-2.5 rounded text-sm transition-all duration-150 border ${isActive ? 'bg-red-50 text-red-500 border-red-100 ' : 'text-slate-500 hover:bg-slate-50 border-transparent hover:text-slate-700'}`} key={item.name} to={item.path} end = {item.path === '/dashboard'} onClick={()=> setIsOpen(false)}>
+                        <item.icon className={`size-4.5 shrink-0 ${isActive ? 'text-red-500' : 'text-slate-500'}`}/>
+                        {item.name}
+                        {isActive && <span className='ml-auto w-[5px] h-5 rounded-full bg-red-500' />}
+                    </NavLink>
+                )
+            })}
+        </nav>
+
+        {/*  users footer */}
+        <div className='p-4 border-t border-slate-100'>
+            <div className='flex items-center gap-3 p-2 rounded-xl hover:bg-slate-50 transition-colors'>
+                <div className='size-8 rounded-full bg-linear-to-r from-red-400 to-pink-400 flex items-center justify-center text-white text-sm font-medium shrink-0'>
+                    {user?.name?.charAt(0).toUpperCase() || 'U'}
+                </div>
+                <div className='flex-1 min-w-0'>
+                    <div className='text-sm text-slate-800 truncate'>
+                        {user?.name}
+                    </div>
+                    <div  className='text-xs text-slate-400 truncate'>
+                        {user?.email}
+                    </div>
+                </div>
             </div>
-
-             {/*  nav links */}
-            <nav className = 'flex-1 px-3 space-y-1'>
-                {navItems.map((item)=> { 
-                    const isActive = location.pathname === item.path;
-
-                    return (
-                        <NavLink className={`flex items-center gap-3 px-3 py-2.5 rounded text-sm transition-all duration-150 border ${isActive ? 'bg-red-50 text-red-500 border-red-100 ' : 'text-slate-500 hover:bg-slate-50 border-transparent hover:text-slate-700'}`} key={item.name} to={item.path} end = {item.path === '/dashboard'} onClick={()=> setIsOpen(false)}>
-                            <item.icon className={`size-4.5 shrink-0 ${isActive ? 'text-red-500' : 'text-slate-500'}`}/>
-                            {item.name}
-                            {isActive && <span className='ml-auto w-[5px] h-5 rounded-full bg-red-500' />}
-                        </NavLink>
-                    )
-                })}
-            </nav>
+            <button className='mt-1 flex items-center gap-3 px-3 py-2 rounded text-sm text-slate-500 hover:bg-red-50 hover:text-red-500 transition-all duration-150' onClick={logout}>
+                <LogOutIcon className='size-4'/>
+                Sign Out
+            </button>
         </div>
     </div>
   )
