d7ea592cf1aca4a2283b537c5bc0fd84dd77773d
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-06-06 08:46:34 +0600

    complete the nav section in sidebar.tsx page

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { CalendarDaysIcon, LayoutDashboardIcon, UserIcon } from 'lucide-react'
+import { NavLink, useLocation } from 'react-router-dom'
 
-const Sidebar = () => {
+const Sidebar = ({isOpen , setIsOpen} : {isOpen : boolean, setIsOpen : (val : boolean) => void }) => {
+
+    const location = useLocation();
+    const navItems = [
+        {name : 'Dashboard', icon : LayoutDashboardIcon, path : '/dashboard'},
+        {name : 'Accounts', icon : UserIcon, path : '/accounts'},
+        {name : 'Scheduler', icon : CalendarDaysIcon, path : '/schedule'},
+        {name : 'AI-Composer', icon : LayoutDashboardIcon, path : '/ai-composer'},
+    ]
   return (
-    <div>Sidebar</div>
+    <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-slate-200 flex flex-col h-full transform transition-transform duration-200 ease-in-out md:relative md:translate-x-0 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        {/*  logo */}
+        <div className='p-6 pb-4'>
+            <div className='text-xl tracking-tight text-slate-800 flex items-center gap-1.5'>
+                <img src="/logo.svg" alt="logo not found" className='size-6'/>
+                Scheduler
+            </div>
+             {/*  nav section label */}
+            <div className='px-6 py-2'>
+                <span className='text-xs text-slate-500 uppercase tracking-wider'>menu</span>
+            </div>
+
+             {/*  nav links */}
+            <nav className = 'flex-1 px-3 space-y-1'>
+                {navItems.map((item)=> { 
+                    const isActive = location.pathname === item.path;
+
+                    return (
+                        <NavLink className={`flex items-center gap-3 px-3 py-2.5 rounded text-sm transition-all duration-150 border ${isActive ? 'bg-red-50 text-red-500 border-red-100 ' : 'text-slate-500 hover:bg-slate-50 border-transparent hover:text-slate-700'}`} key={item.name} to={item.path} end = {item.path === '/dashboard'} onClick={()=> setIsOpen(false)}>
+                            <item.icon className={`size-4.5 shrink-0 ${isActive ? 'text-red-500' : 'text-slate-500'}`}/>
+                            {item.name}
+                            {isActive && <span className='ml-auto w-[5px] h-5 rounded-full bg-red-500' />}
+                        </NavLink>
+                    )
+                })}
+            </nav>
+        </div>
+    </div>
   )
 }
 
