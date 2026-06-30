694432d009c7cc024eb16fa9cafbd14f5bb19fd8
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-06-30 10:16:49 +0600

    complete the user model in server site

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
 
@@ -17,6 +17,12 @@ const port = process.env.PORT || 3000;
 app.get('/', (_req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+//Global Error Handler
+app.use((err : any , _req : Request , res : Response , _next : NextFunction)=> {
+    console.log(err);
+    res.status(500).send(err?.response?.data?.message || err.message)
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
