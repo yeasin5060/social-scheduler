7730a9d3611d68a1d034b93c81ec727f2897f91f
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-03 09:23:45 +0600

    complete the  getOrCreateZernioPrifile function in authsocial contriller

---
+++ server/controllers/socilaAuth.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import zernio from "../config/zernio.js";
+import { User } from "../models/User.model.js";
+
+//
+
+const getOrCreateZernioPrifile = async (user : any) : Promise <string> => {
+    try {
+        const result = await zernio.profiles.listProfiles();
+        const data = result.data as any;
+        const profiles : any[] = Array.isArray(data) ? data : data.profiles || data?.data || [] ;
+
+        if(profiles.length > 0){
+            const pid = profiles[0]._id || profiles[0].id;
+            await User.findByIdAndUpdate(user._id , {zernioProfileId : pid});
+            return pid;
+        }
+
+        const createResult = await zernio.profiles.createProfile({
+            body : {name : `${user.name || user.email}' s workspace`} as any,
+        });
+        const create = (createResult.data as any) ?.profiles || createResult.data;
+        const pid = create?._id || create?.id;
+
+        if(!pid){
+            throw new Error('Failed to create nernio profile -no ID returned');
+        }
+
+        await User.findByIdAndUpdate(user._id , {zernioProfileId : pid});
+        return pid;
+    } catch (error : any) {
+        console.log('getOrCreateZernioPrifile Error', error?.message || error);
+        throw error;
+    }
+}
+
+// Generate Auth authentication Url
+//Get /api/auth/platform
+
+
+export const generateAuthUrl = async (req: Request , res: Response) : Promise <void> => {
+    try {
+        const {platform} = req.params;
+    } catch (error : any) {
+        res.status(500).json({ message : error?.message || "Server Error" });
+    }
+}
