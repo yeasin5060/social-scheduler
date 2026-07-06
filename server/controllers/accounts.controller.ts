d9b44eb82160449321774a99f326800c59c1513e
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-06 08:24:37 +0600

    complete tht disconnetedAccount controller

--- server/controllers/accounts.controller.ts
+++ server/controllers/accounts.controller.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { AuthRequest } from "../middlewares/auth.middleware.js";
 import { Account } from "../models/Accounts.model.js";
+import zernio from "../config/zernio.js";
 
 //get accounts 
 // get /api/accounts
@@ -14,7 +15,7 @@ export const getAccounts = async (req: AuthRequest , res: Response ) : Promise <
     }
 }
 
-//get accounts 
+//post accounts 
 // post /api/accounts
 
 export const addAccount = async (req: AuthRequest , res: Response ) : Promise <void> => {
@@ -22,6 +23,31 @@ export const addAccount = async (req: AuthRequest , res: Response ) : Promise <v
         const {platform, handle, avatarUrl} = req.body;
         const account = await Account.create({user : req.user._id , platform, handle, avatarUrl});
         res.status(201).json(account)
+    } catch (error : any) {
+        res.status(500).json({ message : error?.message || "Server Error" });
+    }
+}
+
+//dicconnected accounts 
+// delete /api/accounts
+
+export const disconnectedAccount = async (req: AuthRequest , res: Response ) : Promise <void> => {
+    try {
+        const account = await Account.findOne({ _id : req.params.id , user : req.user._id ,});
+        if(!account){
+            res.status(404).json({message : "Account not found"});
+            return;
+        }
+        if(account.zernioAccountId){
+            try {
+                await zernio.accounts.deleteAccount({path : {accountId : account.zernioAccountId}})
+            } catch (error : any) {
+                res.status(500).json({ message : error?.response?.data?.message || error?.message});
+            }
+        }
+
+        await account.deleteOne()
+        res.json({message : "Account disconneted successfully"})
     } catch (error : any) {
         res.status(500).json({ message : error?.message || "Server Error" });
     }
