dbd35f40b17d10b66f3da994e369042205916b15
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-06 08:09:33 +0600

    complete tht addAccount controller

--- server/controllers/accounts.controller.ts
+++ server/controllers/accounts.controller.ts
@@ -3,13 +3,25 @@ import { AuthRequest } from "../middlewares/auth.middleware.js";
 import { Account } from "../models/Accounts.model.js";
 
 //get accounts 
-
 // get /api/accounts
 
 export const getAccounts = async (req: AuthRequest , res: Response ) : Promise <void> => {
     try {
         const accounts = await Account.find({user : req.user._id});
         res.json(accounts)
+    } catch (error : any) {
+        res.status(500).json({ message : error?.message || "Server Error" });
+    }
+}
+
+//get accounts 
+// post /api/accounts
+
+export const addAccount = async (req: AuthRequest , res: Response ) : Promise <void> => {
+    try {
+        const {platform, handle, avatarUrl} = req.body;
+        const account = await Account.create({user : req.user._id , platform, handle, avatarUrl});
+        res.status(201).json(account)
     } catch (error : any) {
         res.status(500).json({ message : error?.message || "Server Error" });
     }
