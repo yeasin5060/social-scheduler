cd20230eb2a3f47c0f2ab0392fe032c85db303a7
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-20 09:08:08 +0600

    fetch the login route in login.tsx client site

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { MailIcon, LockIcon, ArrowRightIcon, User2Icon } from "lucide-react";
+import { useAuth } from "../context/AuthContext";
+import api from "../api/axios";
+import toast from "react-hot-toast";
 
 export default function Login() {
     const [loginState, setLoginState] = useState(true);
@@ -9,16 +12,26 @@ export default function Login() {
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const {login, user} = useAuth()
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-            navigate("/dashboard");
-        }, 1000);
+        try {
+            const {data} = await api.post(`/api/auth ${loginState ? "login" : "register"}`, {name ,email , password});
+            login(data , data.token);
+            navigate ('/dashboard')
+        } catch (error:any) {
+            toast.error(error?.response?.data?.message || error.message)
+        }finally {
+            setLoading(false)
+        }
     };
 
+
+    useEffect(() => {
+        if(user) navigate('/dashboard')
+    },[])
     return (
         <div className="min-h-screen bg-slate-100 flex items-center justify-center p-4">
             <div className="relative w-full max-w-md">
